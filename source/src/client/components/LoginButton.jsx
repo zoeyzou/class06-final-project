06873d24e92569cc666a9fcf45fd270f4841be5d
nirmala <nirmala@surf.net.np>
2019-01-23 22:45:36 +0100

    add button component for logout and login

--- source/src/client/components/LoginButton.jsx
+++ source/src/client/components/LoginButton.jsx
@@ -1,23 +1,20 @@
-import React, { Component } from 'react'
+import React, {Component} from 'react'
+import {Button} from 'evergreen-ui'
 
 export default class LoginButton extends Component {
   render() {
     //checks if user is signed in and render a login or logout button accordingly
     if (!this.props.isAuthenticated) {
       return (
-        <li>
-          <a href='http://localhost:9001/auth/google' className='loginbutton'>
-            login
-          </a>
-        </li>
+        <Button is="a" href="http://localhost:9001/auth/google">
+          login
+        </Button>
       )
     } else {
       return (
-        <li>
-          <a href='http://localhost:9001/auth/google/logout' className='loginbutton'>
-            logout
-          </a>
-        </li>
+        <Button is="a" href="http://localhost:9001/auth/google/logout">
+          logout
+        </Button>
       )
     }
   }
